19bdce05d21e247e2c4e355cdc09fa4adaa5b729
Allen Gonzalez <allengonzalez@allens-mbp.lan>
2022-02-03 12:31:45 -0500

    fixed s3

--- main.tf
+++ main.tf
@@ -64,12 +64,12 @@ resource "aws_s3_bucket" "s3_bucket" {
   }
 }
 
-resource "aws_bucket_public_access_block" "s3_bucket" {
+resource "aws_s3_bucket_public_access_block" "s3_bucket" {
   bucket = aws_s3_bucket.s3_bucket.id
 
   block_public_acls       = true
   block_public_policy     = true
-  ignore_public_acl       = true
+  ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
